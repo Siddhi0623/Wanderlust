4019068b85814879d0fbd442fa2e80d4812e0112
Siddhikale <xesiddhikale51@gmail.com>
2026-08-24 23:29:45 +0530

    Password strength

--- controllers/users.js
+++ controllers/users.js
@@ -5,25 +5,63 @@ module.exports.renderSignupForm = (req, res) => {
     res.render("users/signup.ejs");
 }
 
-module.exports.signup = async (req, res) => {
-    try{
-        let{ email, username, password } = req.body;
-    const newUser = new User({ email, username });
-    const registeredUser = await User.register(newUser, password);
-    console.log(registeredUser);
-    req.login(registeredUser, err => {
-        if (err) {
-            return next(err);
-        }
-        req.flash('success', 'Welcome to Wanderlust!');
-        res.redirect('/listings');
-    });
+// module.exports.signup = async (req, res) => {
+//     try{
+//         let{ email, username, password } = req.body;
+//     const newUser = new User({ email, username });
+//     const registeredUser = await User.register(newUser, password);
+//     console.log(registeredUser);
+//     req.login(registeredUser, err => {
+//         if (err) {
+//             return next(err);
+//         }
+//         req.flash('success', 'Welcome to Wanderlust!');
+//         res.redirect('/listings');
+//     });
    
-    } catch(e){
+//     } catch(e){
+//         req.flash("error", e.message);
+//         res.redirect('/signup');
+//     }
+// }
+module.exports.signup = async (req, res, next) => {
+    try {
+        let { username, email, password } = req.body;
+
+        // Password requirements
+        const passwordRegex =
+            /^(?=.*[a-z])(?=.*[A-Z])(?=.*\d)(?=.*[@$!%*?&])[A-Za-z\d@$!%*?&]{8,}$/;
+
+        if (!passwordRegex.test(password)) {
+            req.flash(
+                "error",
+                "Password must be at least 8 characters long and contain at least one uppercase letter, one lowercase letter, one number, and one special character."
+            );
+
+            return res.redirect("/signup");
+        }
+
+        const newUser = new User({
+            email,
+            username
+        });
+
+        const registeredUser = await User.register(newUser, password);
+
+        req.login(registeredUser, (err) => {
+            if (err) {
+                return next(err);
+            }
+
+            req.flash("success", "Welcome to Wanderlust!");
+            res.redirect("/listings");
+        });
+
+    } catch (e) {
         req.flash("error", e.message);
-        res.redirect('/signup');
+        res.redirect("/signup");
     }
-}
+};
 
 module.exports.renderLoginForm = (req, res) =>{
     res.render("users/login.ejs");
